e1e55de13760a660474e11525c724ac08e6bcc7c
Bruno Leonardo (github.com/blss-tico) <bruno.ccutp@gmail.com>
2025-01-31 15:25:12 -0300

    feat: Criado função para atualizar a tela com amigos adicionados.

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigoAdicionado;
 let listaDeAmigos = [];
+let inputAdicionarAmigo = document.getElementById('amigo');
+let listaDeAmigosHtml = document.getElementById('listaAmigos');
 
-function adicionarAmigo() {
-  amigoAdicionado = document.getElementById('amigo').value;
-  if (!amigoAdicionado) { alert('Digite o nome do seu amigo para o sorteio'); }
-  listaDeAmigos.push(amigoAdicionado);
+function atualizaListaDeAmigosTela(amigo) {
+  let li = document.createElement('li');
+  li.textContent = amigo;
+  listaDeAmigosHtml.appendChild(li);
 }
 
+function adicionarAmigo() {
+  amigoAdicionado = inputAdicionarAmigo.value;
+  if (!amigoAdicionado) { 
+    alert('Digite o nome do seu amigo para o sorteio'); 
+    return;
+  }
+
+  listaDeAmigos.push(amigoAdicionado);
+  atualizaListaDeAmigosTela(amigoAdicionado);
+  inputAdicionarAmigo.value = '';
+}
