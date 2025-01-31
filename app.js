838c5d1e0bae7f86f69cb8a3df8fdb63f8d6769c
Bruno Leonardo (github.com/blss-tico) <bruno.ccutp@gmail.com>
2025-01-31 15:13:17 -0300

    feat: Criado funcao adicionarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigoAdicionado;
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+  amigoAdicionado = document.getElementById('amigo').value;
+  if (!amigoAdicionado) { alert('Digite o nome do seu amigo para o sorteio'); }
+  listaDeAmigos.push(amigoAdicionado);
+}
+
