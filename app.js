6548bc03a64d7d9fddd48063cff61a441530c885
Bruno Leonardo (github.com/blss-tico) <bruno.ccutp@gmail.com>
2025-01-31 15:58:01 -0300

    feat: Criado função para retirar amigo sorteado da lista de amigos

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let inputAdicionarAmigo = document.getElementById('amigo');
 let listaDeAmigosHtml = document.getElementById('listaAmigos');
 let listaDeResultadoHtml = document.getElementById('resultado');
 
+// função para adicionar um amigo na lista de amigos
+// após a adição o form é limpo e o foco permanece no form
 function adicionarAmigo() {
   amigoAdicionado = inputAdicionarAmigo.value;
   if (!amigoAdicionado) { 
@@ -14,16 +16,20 @@ function adicionarAmigo() {
 
   listaDeAmigos.push(amigoAdicionado);
   atualizaListaPeloIdNoHtml(amigoAdicionado, listaDeAmigosHtml);
+
   inputAdicionarAmigo.value = '';
   inputAdicionarAmigo.focus();
 }
 
+// função para atualizar uma lista no HTML baseado no id do elemento lista(ul)
 function atualizaListaPeloIdNoHtml(dado, listaHtml) {
   let li = document.createElement('li');
   li.textContent = dado;
   listaHtml.appendChild(li);
 }
 
+// função para sortear um amigo
+// após o sorteio aleatório, o amigo é retirado da lista para um novo sorteio
 function sortearAmigo() {
   if (listaDeAmigos.length <= 0) {
     alert('Não há amigos para sortear');
@@ -31,8 +37,13 @@ function sortearAmigo() {
   }
 
   let numeroSorteio = parseInt(Math.random() * listaDeAmigos.length);
-  atualizaListaPeloIdNoHtml(listaDeAmigos[numeroSorteio], listaDeResultadoHtml);
+  let amigoSorteado = listaDeAmigos[numeroSorteio];
   
-  let index = listaDeAmigos.indexOf(listaDeAmigos[numeroSorteio]);
+  atualizaListaPeloIdNoHtml(amigoSorteado, listaDeResultadoHtml);
+  retiraAmigoSortadoDaLista(amigoSorteado);  
+}
+
+function retiraAmigoSortadoDaLista(amigoSorteado) {
+  let index = listaDeAmigos.indexOf(amigoSorteado);
   if (index > -1) { listaDeAmigos.splice(index, 1); }
 }
